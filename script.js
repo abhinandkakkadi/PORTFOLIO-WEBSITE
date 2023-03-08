de043542fc89fa3b3e79a0e9f6f918b5a26c7585
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-03-08 15:02:52 +0530

    started adding javascripat to the project

---
+++ script.js
@@ -0,0 +1,24 @@
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const password = document.getElementById("password");
+const cPassword = document.getElementById("confirm-password");
+const submit = document.getElementById("myForm");
+
+// add listener
+submit.addEventListener("submit", (event) => {
+  event.preventDefault();
+  checkValidity();
+});
+
+function checkValidity() {
+  // Name
+  if(name.value )
+}
+
+
+
+
+
+// REGEX STUDIES
+
