dc08106dbd167bd2c152d5ae02576e46124be4db
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-03-09 09:55:21 +0530

    Add form submission functionality

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
 const password = document.getElementById("password");
 const cPassword = document.getElementById("confirm-password");
-const submit = document.getElementById("myForm");
 
 //
 const inputs = document.querySelectorAll("input");
@@ -21,6 +17,7 @@ const patterns = {
 function checkValidate(field, regex) {
   if (regex.test(field.value)) {
     field.className = "form-control valid";
+    field.parentNode.querySelector("p").className = "disappear";
   } else {
     field.className = "form-control invalid";
   }
@@ -34,8 +31,11 @@ inputs.forEach((input) => {
       if (e.target.value === password.value) {
         e.target.className = "form-control valid";
         password.className = "form-control valid";
+
+        password.parentNode.querySelector("p").className = "disappear";
       } else {
         e.target.className = "form-control invalid";
+        password.className = "form-control invalid";
       }
     } else if (e.target != password) {
       checkValidate(e.target, patterns[e.target.attributes.name.value]);
@@ -43,6 +43,40 @@ inputs.forEach((input) => {
   });
 });
 
-submit.addEventListener("submit", (event) => {
-  event.preventDefault();
-});
+// SUBMISSION PART
+// function sendMail() {
+//   console.log(1000);
+// emailjs.send("service_gnfht1q", "template_qo5vzdg", {
+//   name_send: document.getElementById("name").value,
+//   phone_send: document.getElementById("phone").value,
+//   email_send: document.getElementById("email").value,
+// });
+//
+// }
+// SMTP JS
+
+// Password: "pxlgglialcrlltrk"
+// var submit = document.getElementById("submit");
+// submit.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   var nameSend = document.getElementById("name").value;
+//   var emailSend = document.getElementById("email").value;
+//   var phoneSend = document.getElementById("phone").value;
+//   var body =
+//     "name: " +
+//     nameSend +
+//     "<br/> email: " +
+//     emailSend +
+//     "<br/> phone " +
+//     phoneSend;
+
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "nanduttanvsabhi@gmail.com",
+//     Password: "553B6E3E8BDA288DC2C427BD81576DAF03F4",
+//     To: "nanduttanvsabhi@gmail.com",
+//     From: emailSend,
+//     Subject: "This is the subject",
+//     Body: body,
+//   }).then((message) => alert(message));
+// });
